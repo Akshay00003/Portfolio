267a7919293e8d35eb0f1f945739858b079cd04a
Akshay00003 <akshayprakash4003@gmail.com>
2024-02-01 22:56:31 +0530

    Project Done

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+        <p className="text-xl mt-20">
+        I'm a dedicated PYTHON FULLSTACK stack developer with a unique journey into the world of programming. I've always had a passion for solving complex problems and building things from scratch. This drive led me to pursue a career in web development, and I haven't looked back since.
+        </p>
+        <br />
+        <p className="text-xl">
+        I specialize in developing full-stack web applications using PYTHON DJANGO. My journey has been a continuous learning process, and I'm excited to share my experiences and insights with the programming community.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import watch from '../assets/portfolio/watch.png'
+import birthday from '../assets/portfolio/birthday.png'
+import football from '../assets/portfolio/football.png'
+import mess from '../assets/portfolio/mess.png'
+import quiz from '../assets/portfolio/quiz.png'
+import review from '../assets/portfolio/review.png'
+import tour from '../assets/portfolio/tour.png'
+import text from '../assets/portfolio/text.png'
+import blog from '../assets/portfolio/blog.png'
+
+const Portfolio = () => {
+  const handleCodeClick =(url)=>{
+window.open(url,'_blank')
+  }
+  const portfolios = [
+    {
+      id: 1,
+      src: watch,
+      url:'https://github.com/Akshay00003/watch_Ecommerce_UI.git'
+    },
+    {
+      id: 2,
+      src: football,
+      url:'https://github.com/Akshay00003/Football_UI.git'
+    },
+    {
+      id: 3,
+      src: blog,
+      url:'https://github.com/Akshay00003/Blog_App.git'
+    },
+    {
+      id: 4,
+      src: mess,
+      url:'https://github.com/Akshay00003/Mess_app.git'
+    },
+    {
+      id: 5,
+      src: birthday,
+    },
+    {
+      id: 6,
+      src: tour,
+      url:'https://github.com/Akshay00003/TourBlog_app.git'
+    },
+    {
+      id: 7,
+      src: text,
+      url:'https://github.com/Akshay00003/Text_app.git'
+    },
+    {
+      id: 8,
+      src: review,
+      url:'https://github.com/Akshay00003/Review_app.git'
+    },
+    {
+      id: 9,
+      src: quiz,
+      url:'https://github.com/Akshay00003/Quiz_app.git'
+    },
+  ];
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b relative from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work here</p>
+        </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+
+{portfolios.map(({id,src,url})=>(
+  <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+            <img
+              src={src}
+              alt=""
+              className="rounded-md hover:scale-105"
+            />
+
+            <div className="flex items-center justify-center">
+              <button onClick={()=>handleCodeClick(url)} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                Demo
+              </button>
+              <button onClick={()=>handleCodeClick(url)} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                Code
+              </button>
+            </div>
+          </div>
+))}
+
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
