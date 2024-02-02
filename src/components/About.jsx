167380b198c002f6ff1e04ea3ac4fcfb5127e4a9
Akshay00003 <akshayprakash4003@gmail.com>
2024-02-02 09:45:11 +0530

    solve

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
         </div>
         <p className="text-xl mt-20">
-        I'm a dedicated PYTHON FULLSTACK stack developer with a unique journey into the world of programming. I've always had a passion for solving complex problems and building things from scratch. This drive led me to pursue a career in web development, and I haven't looked back since.
+        I'm a dedicated PYTHON FULL STACK developer with a unique journey into the world of programming. I've always had a passion for solving complex problems and building things from scratch. This drive led me to pursue a career in web development, and I haven't looked back since.
         </p>
         <br />
         <p className="text-xl">
