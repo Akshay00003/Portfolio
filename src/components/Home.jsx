10ed16aeef6ef73e6a0c64870f10848ea2c4c269
Akshay00003 <akshayprakash4003@gmail.com>
2024-02-04 21:41:57 +0530

    Abot edited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
          I love to work on web application using technologies like
-         Python, Django, React..
+         React,Node,Express,Python,Django..
           </p>
           <div>
             <Link to="portfolio" smooth duration={500} className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
